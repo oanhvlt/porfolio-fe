840791568e2b95d221b82170f6455f0e578302cd
oanhvlt <ovu1024@gmail.com>
2024-04-26 17:34:58 +0700

    setup alias vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     port: 3000
   },
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      pages: path.resolve(__dirname, "./src/screens"),
+      styles: path.resolve(__dirname, "./src/styles"),
+    },
   },
 })
